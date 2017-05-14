452a01b334aae03ea0403e097e006ab7e8ba180b
stevefsswanson <swanson.stev@gmail.com>
2017-05-14 05:37:17 -0400

    make it work for many convos

--- ranker.py
+++ ranker.py
@@ -4,19 +4,20 @@ import json
 FIREBASE_URL = "https://chatdb-80b85.firebaseio.com/Messages.json"
 PERSAN_URL = 'http://127.0.0.1:5000/PersonaliTyzer'
 
-messages = requests.get(FIREBASE_URL).json()
-
+convos = requests.get(FIREBASE_URL).json()
+print(json.dumps(convos))
 rpc = {}
-for message in messages:
+for convo in convos:
+	print 
 	payload = ""
-	for s in message["Messages"]:
+	for s in convo["Messages"]:
 		if s["Sent"]:
 			payload += s["MessageBody"]
-	rpc["message"] = message
-	rpc["payload"] = payload
+	rpc["message"] = payload
 
-vector = {"Openness":0.5, "Conscientiousness":0.5, "Extraversion":0.5, "Agreeableness":0.5, "Emotional range":0.5, "Curiosity":1}
-matchDist = requests.post(PERSAN_URL, data=json.dumps({"text":payload, "vector":vector}))
+	#vector = {"Openness":0.5, "Conscientiousness":0.5, "Extraversion":0.5, "Agreeableness":0.5, "Emotional range":0.5, 
+	vector = {"Curiosity":1}
+	matchDist = requests.post(PERSAN_URL, data=json.dumps({"text":payload, "vector":vector}))
 
-print rpc
-print matchDist.content
+	print convo['id']
+	print matchDist.content
