0ebf8104024d64af2072245f3dcc7610c3975cfb
stevefsswanson <swanson.stev@gmail.com>
2017-05-14 04:24:42 -0400

    making it rank

--- ranker.py
+++ ranker.py
@@ -1,6 +1,8 @@
 import requests
+import json
 
 FIREBASE_URL = "https://chatdb-80b85.firebaseio.com/Messages.json"
+PERSAN_URL = 'http://127.0.0.1:5000/PersonaliTyzer'
 
 messages = requests.get(FIREBASE_URL).json()
 
@@ -13,4 +15,8 @@ for message in messages:
 	rpc["message"] = message
 	rpc["payload"] = payload
 
+vector = {"Openness":0.5, "Conscientiousness":0.5, "Extraversion":0.5, "Agreeableness":0.5, "Emotional range":0.5}
+matchDist = requests.post(PERSAN_URL, data=json.dumps({"text":payload, "vector":vector}))
+
 print rpc
+print matchDist.content
