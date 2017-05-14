d3bb1220017c61ddf0e8f497d04d09a36aab4311
Helene Peyser <helenepeyser@Helenes-MacBook-Air.local>
2017-05-14 04:07:59 -0400

    ranker dud

--- ranker.py
+++ ranker.py
@@ -4,4 +4,13 @@ FIREBASE_URL = "https://chatdb-80b85.firebaseio.com/Messages.json"
 
 messages = requests.get(FIREBASE_URL).json()
 
-print messages
+rpc = {}
+for message in messages:
+	payload = ""
+	for s in message["Messages"]:
+		if s["Sent"]:
+			payload += s["MessageBody"]
+	rpc["message"] = message
+	rpc["payload"] = payload
+
+print rpc
